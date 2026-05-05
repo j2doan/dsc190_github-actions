b6cedbfc9933f8160b777048935f03f1cf922f5d
j2doan <j2.doan@san.rr.com>
2026-05-04 23:31:32 -0700

    fixed main.py

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import pandas
 
 def main():
     print("Hello from dsc190-github-actions!")
-    unused_variable = 10
-    print("multiple statements"); print("on one line")
+    print("multiple statements")
+    print("on one line")
 
 if __name__ == "__main__":
     main()
