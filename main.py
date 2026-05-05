95c47cbc56468923ff3f2f0d8b9d6314e2001ef6
j2doan <j2.doan@san.rr.com>
2026-05-04 23:27:24 -0700

    broke main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import pandas
+
 def main():
     print("Hello from dsc190-github-actions!")
-
+    unused_variable = 10
+    print("multiple statements"); print("on one line")
 
 if __name__ == "__main__":
     main()
